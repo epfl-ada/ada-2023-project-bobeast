6003fb289d1f388e6421346b9b8944f438179d8a
kamalnour <kamal.nour@epfl.ch>
2023-11-05 16:36:38 +0100

    cleanup

--- .ipynb_checkpoints/helpers-checkpoint.py
+++ .ipynb_checkpoints/helpers-checkpoint.py
@@ -5,6 +5,19 @@ import seaborn as sns
 import scipy.stats as stats
 
 
+def process_interventions(interventions):
+    for col in interventions.columns:
+        if col != 'lang':
+            interventions.loc[:, col] = pd.to_datetime(interventions.loc[:, col],errors='coerce')
+        else :
+            interventions[col]=interventions[col].astype('string')
+    return interventions.copy()
+
+def process_topics_linked(topics_linked):
+    tl=topics_linked.copy()
+    idx=np.where(tl.dtypes==bool)[0]
+    tl[tl.columns[idx]]=tl[tl.columns[idx]].astype(int)
+    return tl
 
 def process_agg_time_series(agg_ts):
     res=pd.DataFrame()
