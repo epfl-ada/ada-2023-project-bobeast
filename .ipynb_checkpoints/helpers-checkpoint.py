97f3f23a3d4f1a6768790cbe623b85073ba797ff
kamalnour <kamal.nour@epfl.ch>
2023-11-05 15:48:50 +0100

    first naive look at aggregated time series

--- .ipynb_checkpoints/helpers-checkpoint.py
+++ .ipynb_checkpoints/helpers-checkpoint.py
@@ -7,7 +7,12 @@ import scipy.stats as stats
 
 
 def process_agg_time_series(agg_ts):
-    res=pd.DataFrame(agg_ts)
+    res=pd.DataFrame()
+    if len(agg_ts.values())==3:
+        res=pd.DataFrame(agg_ts)
+    else:
+        res=pd.DataFrame(agg_ts.values())
+        res.index=agg_ts.keys()
     res.index=pd.to_datetime(res.index)
     res['date']=res.index
     res['year']=res['date'].dt.year
